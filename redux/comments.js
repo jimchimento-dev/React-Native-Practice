4c80b05fed71d3a2cfe79e5de384a84ee37932aa
Jim Chimento <chimento.dev@gmail.com>
2021-11-20 16:47:46 -0500

    Added a new button to CampsiteInfo component when pressed pulls up a modal where users can leave a rating, name and comment that gets added via Redux.

--- redux/comments.js
+++ redux/comments.js
@@ -8,6 +8,12 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
         case ActionTypes.COMMENTS_FAILED:
             return { ...state, errMess: action.payload };
 
+        case ActionTypes.ADD_COMMENT:
+            const comment = action.payload;
+            const id = state.comments.length;
+            const newComment = { ...comment, id };
+            return { ...state, errMess: null, comments: state.comments.concat(newComment) };
+
         default:
             return state;
     }
